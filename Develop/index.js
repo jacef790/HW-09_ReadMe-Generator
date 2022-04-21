8f1147b75c0d381825952eb9929d72f238e945e9
jacef790 <jace.feinstein@live.com>
2022-04-21 02:19:01 -0600

    Initial upload, pseudocode add

---
+++ Develop/index.js
@@ -0,0 +1,46 @@
+// Steps to Solve
+    // 1. User answers questions
+        // inquirer prompt
+    // 2. Pass answers in the .then() 
+        // import generateMarkdown 
+    // 3. Hand of to Generate Markdown to create MD syntax
+    // 4. pass MD to writeTofile
+    // console.log("in writetoFIle", var);
+    // TODO: Include packages needed for this application
+    
+    // TODO: Create an array of questions for user input
+    // const questions = [];
+    
+    // TODO: Create a function to write README file
+    // function writeToFile(fileName, data) {
+        // use fs.writefile here
+        // How do I use the markdown file here??
+            // maybe a rest operator
+            // import markdown functions
+           
+        
+    // }
+    
+    // TODO: Create a function to initialize app
+    function init() {
+        // Write inquirer prompt here
+        // Call writetoFile inside .then()
+         // How to send answers from users into markdown functions??
+        // pass asnswers to writetofile here
+    }
+    
+    // Function call to initialize app
+    init();// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [`What's your GitHub?`, `Email?`, `Project Name?`, `Description of Project?`
+`License Type?`, ``];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
