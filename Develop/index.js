efcac19e2196de1657e55bf8aff1625fef56111e
jacef790 <jace.feinstein@live.com>
2022-04-21 02:40:03 -0600

    Variables, question array added

--- Develop/index.js
+++ Develop/index.js
@@ -7,6 +7,9 @@
     // 4. pass MD to writeTofile
     // console.log("in writetoFIle", var);
     // TODO: Include packages needed for this application
+
+// const inquirer = require("inquirer");
+
     
     // TODO: Create an array of questions for user input
     // const questions = [];
@@ -20,6 +23,49 @@
            
         
     // }
+
+
+    // Inquirer and FS variables
+    const inquirer = require('inquirer');
+    const fs = require('fs');
+    const genMarkdown = require('./utils/generateMarkdown.js')
+
+// question array
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of the project'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What does your application do?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license does your project use',
+        choices: ['None', 'MIT', 'Apache 2.0', 'GNU General Public License v3.0']
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username'
+    },
+    {
+        type: 'list',
+        name: 'wizard',
+        message: 'Are you a magical wizard',
+        choices: ['Yes', 'No']
+    }
+]
     
     // TODO: Create a function to initialize app
     function init() {
