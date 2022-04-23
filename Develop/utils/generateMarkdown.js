844f0c13d85f2e3049e0042beddf455d01e677de
jacef790 <jace.feinstein@live.com>
2022-04-22 21:00:27 -0600

    license badge rendering

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -6,17 +6,19 @@ const index = require('../index.js');
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  let licenseBadge = '';
-  if(license === 'MIT') {
-      licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-  } if (license === 'Apache 2.0') {
-      licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-  } if (license === 'GNU General Public License v3.0') {
-      licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
-  } else {
-    licenseBadge = ''
+  let licenseBadge = ''
+  if (license === "MIT") {
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
   }
-  return licenseBadge
+  if (license === "GPLv2") {
+    return `[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`
+  }
+  if (license === "Apache") {
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+  }
+  else {
+    return licenseBadge
+  }
 }
 
 
@@ -63,10 +65,10 @@ function generateMarkdown(data) {
   # ${data.title}
 
   # ${renderLicenseSection(data.license)} 
+  
+  # ${renderLicenseBadge(data.license)}
 
   # ${renderLicenseLink(data.license)}
-
-  # ${renderLicenseBadge(data.license)}
 
   # ${data.description}
 
@@ -83,9 +85,6 @@ function generateMarkdown(data) {
   # Usage-Free use
 
   # Questions-Reach me at https://github.com/jacef790 or jace.feinstein0218@gmail.com for questions
-  
-  
-
 `;
 }
 
